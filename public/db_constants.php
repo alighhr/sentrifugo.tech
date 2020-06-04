53ae6b13a5e8745ad34a1f54a054e58cba8d6683
Ubuntu <ubuntu@ip-172-31-21-226.ap-southeast-1.compute.internal>
2020-06-04 10:37:58 +0000

    Initial commit

---
+++ public/db_constants.php
@@ -0,0 +1,7 @@
+<?php
+	           defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','localhost');
+	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','root');
+	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','d8k@qxa_HbY!Y6A$tgv%pFSN#BHSL');
+	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','sentrifugo-crm');
+	           
+	         ?>
